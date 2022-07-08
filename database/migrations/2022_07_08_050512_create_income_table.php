164ea925bc1a7763c4fc4b2230b19061a4b8d206
Phue-Pwint-Phway <scm.phuepwintphway@gmail.com>
2022-07-08 16:48:32 +0630

    [add] existing project file to git (ngwethonesaryin=>initial files)

---
+++ database/migrations/2022_07_08_050512_create_income_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('income', function (Blueprint $table) {
+            $table->id();
+            $table->integer('income');
+            $table->integer('loans');
+            $table->integer('expenses');
+            $table->integer('savings');
+            $table->integer('created_user_id');
+            $table->integer('updated_user_id');
+            $table->integer('deleted_user_id');
+            $table->timestamps();
+            $table->timestamp('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('income');
+    }
+}
